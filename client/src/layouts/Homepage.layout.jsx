a1e0a57d83678fd10317aa805b829935f0e65325
1JayeshPatil <jayeshpatil2224@gmail.com>
2022-11-17 18:49:13 +0530

    completed foot tab and completed food category cards and carousel

--- client/src/layouts/Homepage.layout.jsx
+++ client/src/layouts/Homepage.layout.jsx
@@ -4,16 +4,16 @@ import FoodTab from "../components/FoodTab";
 
 const HomepageLayout =
   (Component) =>
-    ({ ...props }) => {
-      return (
-        <>
-          <Navbar />
-          <FoodTab />
-          <div className="container mx-auto px-4 lg:px-20">
-            <Component {...props} />
-          </div>
-        </>
-      );
-    };
+  ({ ...props }) => {
+    return (
+      <>
+        <Navbar />
+        <FoodTab />
+        <div className="container mx-auto px-4 lg:px-20">
+          <Component {...props} />
+        </div>
+      </>
+    );
+  };
 
 export default HomepageLayout;
